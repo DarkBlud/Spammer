f47356ca6dc17c5094fd52850c130766db0b8b83
DarkBlud <73785215+DarkBlud@users.noreply.github.com>
2022-03-11 02:07:05 +0330

    Add files via upload

---
+++ Spammer.py
@@ -0,0 +1,25 @@
+#import library
+
+
+from itertools import count
+import pyautogui as pg 
+import time 
+import os
+
+#Get Data
+
+
+counter = int(input("Please Enter count of spamming message :) ( Do not open any other page just stay on that page you want ) : "))
+msg = input("Enter your message : ")
+
+#Action
+
+
+os.system("start C:/Users/DarkBlud/Desktop/spamm/Terminal.py")
+time.sleep(10)
+for i in range(counter): 
+    pg.write(msg)
+    pg.press("Enter")
+
+#IG : DarkBlud
+#Github : DarkBlud
