17fb575cbd5e2e802154c8a7f0db3bd0df01284e
DarkBlud <73785215+DarkBlud@users.noreply.github.com>
2022-03-11 02:07:55 +0330

    Add files via upload

---
+++ Terminal.py
@@ -0,0 +1,27 @@
+#import part
+
+
+import os
+import time
+
+
+#set var
+
+
+a = os.system
+a("title Terminal")
+a("color 0C")
+x = 10
+
+#Action
+
+
+for i in range(10):
+    print(f"Time remain : {x}")
+    time.sleep(1)
+    x += -1
+    a("cls")
+    
+
+#Ig : DarkBlud
+#Github : DarkBlud
