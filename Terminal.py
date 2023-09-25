d9d24ca1c302573e734f899c9e607c74bc70a893
DarkBlud <73785215+DarkBlud@users.noreply.github.com>
2023-09-25 09:19:25 -0400

    Update Terminal.py --linux edition
    Added OS detection

--- Terminal.py
+++ Terminal.py
@@ -3,25 +3,47 @@
 
 import os
 import time
-
+import platform 
 
 #set var
+system = platform.system()
+
+def windows():
+    cmd = os.system
+    cmd("title Terminal")
+    cmd("color 0C")
+    x = 10
+
+    #Action
 
 
-a = os.system
-a("title Terminal")
-a("color 0C")
-x = 10
-
-#Action
-
-
-for i in range(10):
-    print(f"Time remain : {x}")
-    time.sleep(1)
-    x += -1
-    a("cls")
+    for i in range(10):
+        print(f"Time remain : {x}")
+        time.sleep(1)
+        x += -1
+        cmd("cls")
+        cmd('exit')
     
+
+def linux():
+    ter = os.system 
+    ter("clear")
+    timer = 10 
+    for i in range(10):
+        print(f"Time remain : {timer}")
+        time.sleep(1)
+        timer += -1 
+        ter('clear')
+        ter('exit')
+    
+
+if system == "Windows" : 
+    windows()
+elif system == "Linux" : 
+    linux()
+else : 
+        print("This program can't run on your system \n Please use Windows or Linux <3")
+
 
 #Ig : dark._.blud
 #Github : DarkBlud
