b91e9f4a78a07ddbf943d3671fe84e82d789e122
DarkBlud <73785215+DarkBlud@users.noreply.github.com>
2023-09-25 09:18:49 -0400

    Update Spammer.py -linux edition
    added new function that detect the OS

--- Spammer.py
+++ Spammer.py
@@ -1,31 +1,59 @@
 #import library
-
-
 from itertools import count
+import platform
 import pyautogui as pg 
 import time 
 import os
-os.system("pip install -r requirements.txt && cls")
+# os.system("pip install -r requirements.txt && clear")
 
-#Warning 
-os.system("color 0C") 
-print("Important ;; Do not change the page until the proggrame finish")
-input("Press Any thing to continue ...")
-os.system("color 02 && cls")  
-#Get Data
+system = platform.system() 
 
-
-counter = int(input("Enter count of message  : "))
-msg = input("Enter your message : ")
-
-#Action
+def windows():
+    #Warning 
+    os.system("color 0C && cls") 
+    print("Important ;; Do not change the page until the program finish")
+    input("Press Any thing to continue ...")
+    os.system("color 02 && cls")  
+    #Get Data
 
 
-os.system("start Terminal.py")
-time.sleep(10)
-for i in range(counter): 
-    pg.write(msg)
-    pg.press("Enter")
+    counter = int(input("Enter count of message  : "))
+    msg = input("Enter your message : ")
 
+    #Action
+
+
+    os.system("start Terminal.py")
+    time.sleep(10)
+    for i in range(counter): 
+        pg.write(msg)
+        pg.press("Enter")
+    os.system("cls")
+    windows()
+
+def linux():
+    os.system('clear')
+    os.system("pip install -r requirements.txt && clear") 
+    os.system('clear')  
+    print("Important ;; Do not change the page until the proggrame finish")
+    input("Press Any thing to continue ...")
+    os.system("clear")
+    counter = int(input("Enter count of message  : "))
+    msg = input("Enter your message : ")
+    os.system("gnome-terminal -- python Terminal.py")
+    time.sleep(10)
+    for i in range(counter): 
+        pg.write(msg)
+        pg.press("Enter")
+    os.system('clear')
+    linux()
+
+
+if system == "Linux":
+    linux()
+elif system == "Windows":
+    windows()
+else:
+    print("This program can't run on your system \n Please use Windows or Linux <3")
 #IG : dark._.blud
 #Github : DarkBlud
